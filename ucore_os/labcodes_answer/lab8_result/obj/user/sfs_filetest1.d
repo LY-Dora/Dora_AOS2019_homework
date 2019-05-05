dc4bbcc1637cf332812ca27a7b0af31a6c6f93dd
Yuan Li <lydorazoe@gmail.com>
2019-05-05 15:55:27 +0800

    update

--- ucore_os/labcodes_answer/lab8_result/obj/user/sfs_filetest1.d
+++
@@ -1,4 +0,0 @@
-obj/user/sfs_filetest1.o obj/user/sfs_filetest1.d: user/sfs_filetest1.c \
- user/libs/ulib.h libs/defs.h libs/stdio.h libs/stdarg.h libs/stdlib.h \
- libs/string.h libs/stat.h user/libs/file.h user/libs/dir.h libs/dirent.h \
- libs/unistd.h
